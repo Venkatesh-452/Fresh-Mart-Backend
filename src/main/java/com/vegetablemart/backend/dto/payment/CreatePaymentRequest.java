e47fdc1ae1dbdb45252e6e1d77ddb0104256d1da
Venkatesh-452 <luckyvenkat452@gmail.com>
2026-09-02 17:11:59 +0530

    Add positive validation to payment order ID

--- src/main/java/com/vegetablemart/backend/dto/payment/CreatePaymentRequest.java
+++ src/main/java/com/vegetablemart/backend/dto/payment/CreatePaymentRequest.java
@@ -2,6 +2,7 @@ package com.vegetablemart.backend.dto.payment;
 
 import com.vegetablemart.backend.entity.PaymentMethod;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 @Getter
@@ -12,6 +13,7 @@ import lombok.*;
 public class CreatePaymentRequest {
 
     @NotNull(message = "Order ID is required")
+    @Positive(message = "Order ID must be positive")
     private Long orderId;
 
     @NotNull(message = "Payment method is required")
