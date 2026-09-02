2d2e8d14f0bd719fb5e04b0437dfe55dc896de4d
Venkatesh-452 <luckyvenkat452@gmail.com>
2026-09-02 17:12:05 +0530

    Validate payment transaction ID length

--- src/main/java/com/vegetablemart/backend/dto/payment/PaymentStatusRequest.java
+++ src/main/java/com/vegetablemart/backend/dto/payment/PaymentStatusRequest.java
@@ -1,6 +1,7 @@
 package com.vegetablemart.backend.dto.payment;
 
 import com.vegetablemart.backend.entity.PaymentStatus;
+import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
@@ -14,5 +15,6 @@ public class PaymentStatusRequest {
     @NotNull(message = "Payment status is required")
     private PaymentStatus status;
 
+    @Size(max = 150, message = "Transaction ID must not exceed 150 characters")
     private String transactionId;
 }
